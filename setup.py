381623eaa4a451f8b0b0a15dfb3a8c44c78bb087
Jeremy Dimond <jeremy@jeremydimond.com>
2019-11-10 09:45:08 -0600

    setup.py added jeremydimond namespace prefix to pypi name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-hello-world",
+    name="jeremydimond.python-hello-world",
     version="0.0.1",
     author="Jeremy Dimond",
     author_email="jeremy@jeremydimond.com",
